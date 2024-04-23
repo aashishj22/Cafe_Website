7558641ff50cee1d4f2ff393c5707fa3ab880626
railWorld123 <railworldoffice@gmail.com>
2024-04-23 11:34:00 +0530

    upload

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import "./Carousel.css";
+import CarouselData from "../../JsonFile/Carousel.json"
+import { Card } from '../Card/Card';
+import nextBtn from "../../assets/Sunset/nextArrow.svg"
+import prevBtn from "../../assets/Sunset/prevArrow.svg";
+export const Carousel = () => {
+    const[count, setCount] =useState(0);
+
+    function handleClickPrev(){
+
+    }
+    function handleClickNext(){
+        
+    }
+    var imageNo = 0;
+    // let btnNext = document.getElementById("next");
+    // let btnPrev = document.getElementById("prev");
+    var slider = document.getElementsByClassName("slider");
+    function handleClickPrev(){
+        if(imageNo<1){
+            imageNo=4;
+        }else{
+            imageNo -= 1;
+        }
+        slider[0].style.transform="translateX(" + (imageNo)*(-14) + "%)";
+    }
+    function handleClickNext(){
+        if(imageNo<4){
+            imageNo+=1;
+        }else{
+            imageNo = 0;
+        }
+        slider[0].style.transform="translateX(" + (imageNo)*(-14) + "%)";
+    }
+
+
+
+
+  return (
+<>
+<div className="sub-right-container">
+
+<div className="carousel-container">
+    <div className="control-btns">
+        <button className="btn-1" onClick={handleClickPrev} id="prev"><img src={prevBtn}></img></button>
+        <button className="btn-1" onClick={handleClickNext} id="next"><img src={nextBtn}></img></button>
+    </div>
+    <div className="carousel-heading">
+        <p>We Are Known for :-</p>
+    </div>
+    <div className="slider-wrapper">
+        <div className="slider">
+            { CarouselData.map((x)=>{
+                // console.log(x.id);
+                return(
+                    <Card key={x.id} image={x.image} heading={x.heading} para={x.para} />
+                )
+            })
+
+            }
+            </div>
+    </div>
+
+</div>
+</div>
+</>
+  )
+}
